461865b08947fb5bd9153eac2fa684be6bca7602
mdakeel <aakiltayyab@gmail.com>
2024-04-13 12:58:05 +0530

    scroll bar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,29 +13,29 @@ export const Navbar = () => {
   return (
     <div className='w-full fixed top-0 z-10'>
     <div className='flex justify-between px-[25px] z-10 bg-white '>
-        <div className='flex items-center cursor-pointer'>
-        <AiOutlineMenu  size={"24px"} />
+        <div className='flex items-center space-x-3 cursor-pointer'>
+        <AiOutlineMenu  size={"22px"} className='text-gray-600' />
         <img src={logo} width={"115px"} alt="" className='px-3'/>
         </div>
-              <div className="flex items-center w-[40%] m-2 space-x-4 cursor-pointer"> 
+              <div className="flex items-center w-[44%] m-2 space-x-4 cursor-pointer"> 
               <div className='flex w-full '>          
                 <input
                   type="search"
                   id="search"
-                  className="w-full  py-2 px-4 border border-gray-300 outline-none rounded-l-full "
+                  className="w-full   px-4 border  py-[8px] border-gray-300 outline-none rounded-l-full "
                   placeholder="Search "
                   required
                 />
-                 <button className=" px-4 py-2 border border-gray-300 rounded-r-full bg-gray-50 hover:bg-gray-100 cursor-pointer"><CiSearch size={"24px"} className='text-gray-600' /></button>
+                 <button className=" px-4 py-[8px] border border-gray-300 rounded-r-full bg-gray-50 hover:bg-gray-100 cursor-pointer"><CiSearch size={"24px"} className='text-gray-600' /></button>
                  </div>  
-                 <div className="bg-gray-100 p-[8px] rounded-full hover:bg-gray-200" ><CiVideoOn size={"24px"} className='text-gray-600'/></div>
+                 <div className="bg-gray-100 p-[10px] rounded-full hover:bg-gray-200" ><CiVideoOn size={"20px"} className='text-gray-600'/></div>
                  
               </div>
               
         <div className='flex justify-between items-center w-[10%]'>
-          <div className='cursor-pointer hover:bg-gray-100 rounded-full p-2 '><CiVideoOn size={"24px"} className='w-6 h-6'/></div>
+          <div className='cursor-pointer hover:bg-gray-100 rounded-full p-[8px] '><CiVideoOn size={"24px"} /></div>
         
-          <div className='cursor-pointer hover:bg-gray-100 rounded-full p-2 '><IoIosNotificationsOutline size={"24px"} className='w-6 h-6' /></div>
+          <div className='cursor-pointer hover:bg-gray-100 rounded-full p-[8px] '><IoIosNotificationsOutline size={"24px"} /></div>
         <img src={profile} alt="" className='rounded-full w-8 h-8 cursor-pointer'/>
         </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 // images
 import short from "../assets/short.png"
 import profile from "../assets/profile-img.png"
+import azra from "../assets/azra.png"
 
 // react icons
 import { CiHome } from "react-icons/ci";
@@ -15,11 +16,30 @@ import { MdOutlineWatchLater } from "react-icons/md";
 import { GrCut } from "react-icons/gr";
 import { GoChevronDown } from "react-icons/go";
 import { IoIosArrowForward } from "react-icons/io";
+import { PiFireBold } from "react-icons/pi";
+import { AiOutlineShopping } from "react-icons/ai";
+import { IoMusicalNoteOutline } from "react-icons/io5";
+import { MdOutlineMovie } from "react-icons/md";
+import { MdLiveTv } from "react-icons/md";
+import { IoGameControllerOutline } from "react-icons/io5";
+import { HiOutlineNewspaper } from "react-icons/hi2";
+import { TfiCup } from "react-icons/tfi";
+import { AiOutlineBulb } from "react-icons/ai";
+import { TbHanger } from "react-icons/tb";
+import { MdOutlinePodcasts } from "react-icons/md";
+import { FaYoutube } from "react-icons/fa6";
+import { SiYoutubestudio } from "react-icons/si";
+import { SiYoutubemusic } from "react-icons/si";
+import { TbBrandYoutubeKids } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
+import { RiFlagLine } from "react-icons/ri";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { RiFeedbackLine } from "react-icons/ri";
 
 export const Sidebar = () => {
 
     const sidebarItems = [
-        { icons: <CiHome size={"20px"} />, title: "Home"},
+        { icons: <CiHome size={"22px"} />, title: "Home"},
         { icons: <img src={short} alt="" className='w-5 '/>, title: "Shorts"},
         { icons: <MdOutlineSubscriptions size={"20px"} className='text-gray-600' />, title: "Subscriptions"}
     ]
@@ -27,35 +47,51 @@ export const Sidebar = () => {
     const sidebarYou = [
         {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Your Channel"},
         {icons: <GoHistory size={"20px"} />, title: "History"},
-        {icons: <GoVideo size={"20px"}  className='text-gray-700' />, title: "Your videos"},
-        {icons: <MdOutlineWatchLater size={"20px"} className='text-gray-700' />, title: "Watch later"},
+        {icons: <GoVideo size={"19px"}  className='text-gray-700' />, title: "Your videos"},
+        {icons: <MdOutlineWatchLater size={"22px"} className='text-gray-700' />, title: "Watch later"},
         {icons: <GrCut size={"20px"} className='text-gray-700' />, title: "Your clips"},
         {icons: <GoChevronDown size={"20px"} />, title: "Show more"},
         
     ]
 
-    const sidebarSub = [
+    const sidebarSubscriptions = [
         {icons: <img src={profile} alt='' className='rounded-full w-6 h-6' />, title: "Know Your Creator"},
-        {icons: <img src={profile} alt='' className='rounded-full w-6 h-6' />, title: "Know Your Creator"},
+        {icons: <img src={azra} alt='' className='rounded-full w-6 h-6' />, title: "Azra Naaz"},
     ]
 
     const sidebarExplore = [
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Trending"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Shopping"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Music"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Movies"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Live"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Gaming"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "News"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Sports"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Courses"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Fashion & Beauty"},
-        {icons: <VscAccount size={"20px"} className='text-gray-700' />, title: "Podcasts"},
+        {icons: <PiFireBold size={"22px"} className='text-gray-700' />, title: "Trending"},
+        {icons: <AiOutlineShopping size={"22px"} className='text-gray-700' />, title: "Shopping"},
+        {icons: <IoMusicalNoteOutline size={"22px"} className='text-gray-700' />, title: "Music"},
+        {icons: <MdOutlineMovie size={"20px"} className='text-gray-700' />, title: "Movies"},
+        {icons: <MdLiveTv size={"20px"} className='text-gray-700' />, title: "Live"},
+        {icons: <IoGameControllerOutline size={"20px"} className='text-gray-700' />, title: "Gaming"},
+        {icons: <HiOutlineNewspaper size={"20px"} className='text-gray-700' />, title: "News"},
+        {icons: <TfiCup size={"19px"} className='text-gray-700' />, title: "Sports"},
+        {icons: <AiOutlineBulb size={"20px"} className='text-gray-700' />, title: "Courses"},
+        {icons: <TbHanger size={"20px"} className='text-gray-700' />, title: "Fashion & Beauty"},
+        {icons: <MdOutlinePodcasts size={"20px"} className='text-gray-700' />, title: "Podcasts"},
+    ]
+
+    const fromMoreYoutube = [
+        {icons: <FaYoutube size={"22px"} className='text-[#FF0000]' />, title: "YouTube Premium"},
+        {icons: <SiYoutubestudio size={"21px"} className='text-[#FF0000]' />, title: "YouTube Studio"},
+        {icons: <SiYoutubemusic size={"21px"} className='text-[#ff0000]' />, title: "YouTube Music"},
+        {icons: <TbBrandYoutubeKids size={"22px"} className='text-[#ff0000]' />, title: "YouTube Kids"},
+        
+    ]
+
+    const sidebarSettings = [
+        {icons: <IoSettingsOutline size={"22px"} className='text-gray-700' />, title: "Settings"},
+        {icons: <RiFlagLine size={"20px"} className='text-gray-700' />, title: "Report history"},
+        {icons: <IoIosHelpCircleOutline size={"23px"} className='text-gray-700' />, title: "Help"},
+        {icons: <RiFeedbackLine size={"20px"} className='text-gray-700' />, title: "Send feddback"},
+        
     ]
 
   return (
     <div>
-            <div className='absolute h-[calc(100vh-3.8rem)] w-[16%] left-0  mt-[60px] overflow-y-scroll overflow-x-hidden '>
+            <div className='absolute h-[calc(100vh-3.8rem)] w-[16%] left-0 mt-[60px] overflow-y-scroll overflow-x-hidden '>
                 {/* Main menu */}
                 <div className='m-4'>
                     {
@@ -91,7 +127,7 @@ export const Sidebar = () => {
                 <div className='flex ml-6 mt-4 text-[17px] items-center font-medium cursor-pointer'><h3>Subscriptions</h3></div>
                 <div className='m-4'>
                     {
-                        sidebarSub.map((items, index) => {
+                        sidebarSubscriptions.map((items, index) => {
                             return(
                                 <div key={index} className='flex items-center hover:bg-gray-100 px-2 space-x-6 py-2 rounded-md cursor-pointer '>
                                     {items.icons}
@@ -118,7 +154,50 @@ export const Sidebar = () => {
                     }
                 </div>  
                 <hr />
-        </div>
+
+                {/* More From Youtube */}
+                <div className='flex ml-6 mt-4 text-[17px] items-center font-medium cursor-pointer'><h3>More from YouTube</h3></div>
+                <div className='m-4'>
+                    {
+                        fromMoreYoutube.map((items, index) => {
+                            return(
+                                <div key={index} className='flex  items-center hover:bg-gray-100 px-2 space-x-6 py-2 rounded-md cursor-pointer '>
+                                    {items.icons}
+                                    <p className='text-[14px]  '>{items.title}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>  
+                <hr />
+
+                {/* Youtube settings */}
+                <div className='m-4'>
+                    {
+                        sidebarSettings.map((items, index) => {
+                            return(
+                                <div key={index} className='flex  items-center hover:bg-gray-100 px-2 space-x-6 py-2 rounded-md cursor-pointer '>
+                                    {items.icons}
+                                    <p className='text-[14px]  '>{items.title}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>  
+                <hr />
+
+                {/* about privacy */}
+                <div className='m-4 space-y-4'>
+                <p className='text-[13px] font-semibold text-gray-600' >About Press Copyeight 
+                    Contact Us Creators
+                    Advertise Developers
+                </p>
+                <p className='text-[13px] font-semibold text-gray-600'>About Press Copyeight 
+                    Contact Us Creators
+                    Advertise Developers
+                </p>
+                </div>
+            </div>
     </div>
   )
 }
