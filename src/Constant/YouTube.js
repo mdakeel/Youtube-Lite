f510971a04e87b84a346c221072b466a57f1d719
mdakeel <aakiltayyab@gmail.com>
2024-04-18 00:50:28 +0530

    New API

--- src/Constant/YouTube.js
+++ src/Constant/YouTube.js
@@ -1,11 +1,11 @@
 // export const API_KEY = "AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8";
-// export const BASE_URL = "https://www.googleapis.com/youtube/v3";
-// export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&myRating=like&regionCode=IN&key=${API_KEY}`;
-// export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-// export default API_KEY; // default export
+// // export const BASE_URL = "https://www.googleapis.com/youtube/v3";
+// // export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&myRating=like&regionCode=IN&key=${API_KEY}`;
+// // export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+// // export default API_KEY; // default export
 
 // named export 
-export const API_KEY = "AIzaSyA_L3G46mLilTgelWYFvfixkAscmnHj6IU";
+export const API_KEY = "AIzaSyAVGsZRlwb1xsOtwbDD7WMajpLQ1saEYA0";
 export const BASE_URL = "https://www.googleapis.com/youtube/v3"
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
 export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
